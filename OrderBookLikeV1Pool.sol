6e5421802998733174d46d25529071201d03fe7b
asardon <as@myso.finance>
2022-06-21 23:17:36 +0200

    added groupClaims func

--- OrderBookLikeV1Pool.sol
+++ OrderBookLikeV1Pool.sol
@@ -26,11 +26,19 @@ contract Contract {
 
     struct LoanInfo {
         uint256 lpSnapshot;
-        uint256 collAndRepayAmounts;
+        uint128 repayment;
+        uint128 collateral;
         uint128 totalWeights;
         address borrower;
         uint32 expiry;
         bool repaid;
+    }
+
+    struct GroupedClaimsInfo {
+        uint128 repayments;
+        uint128 collateral;
+        uint128 groupWeight;
+        uint128 maxLoanIdx;
     }
 
     uint128 public loanIdx;
@@ -42,6 +50,7 @@ contract Contract {
     mapping(uint256 => LpInfo) slotIdxToLpInfo;
     mapping(uint256 => LoanInfo) loanIdxToLoanInfo;
     mapping(bytes32 => uint256) lpToMaxLoanIdxClaimed;
+    mapping(bytes32 => GroupedClaimsInfo) slotsToClaimsInfo;
 
     constructor(uint256 _maxLoanPerColl, uint256 _initMinLpAmount, uint256 _minAPR, uint256 _scaleAPR) {
         require(_maxLoanPerColl > 0, "invalid max. borrowable amount");
@@ -59,7 +68,7 @@ contract Contract {
     }
 
     function addLiquidity(uint256 _slotIdx, uint128 _weight) external {
-        require(_slotIdx < 256, "invalid lp slot");
+        require(_slotIdx < 256, "_slotIdx out of bounds");
         require(_weight > 0, "weights > 0");
         LpInfo storage lpInfo = slotIdxToLpInfo[_slotIdx];
         require(lpInfo.addr == address(0) || (lpInfo.removedLiquidity && lpInfo.reassignableAfter < block.number), "lp slot alreay in use");
@@ -76,24 +85,26 @@ contract Contract {
     }
 
     function removeLiquidity(uint256 _slotIdx) external {
+        require(_slotIdx < 256, "_slotIdx out of bounds");
         LpInfo storage lpInfo = slotIdxToLpInfo[_slotIdx];
         require(lpInfo.addr != address(0), "empty lp slot");
         require(lpInfo.addr == msg.sender, "unauthorized lp slot");
         require(!lpInfo.removedLiquidity, "already removed liquidity");
-        require(block.number > lpInfo.earliestRemove, "before min. lping period");
+        uint256 blockNum = block.number;
+        require(blockNum > lpInfo.earliestRemove, "before min. lping period");
         uint256 liquidityRemoved;
         lpSlots &= ~(uint256(1) << _slotIdx);
         liquidityRemoved = lpInfo.weight * initMinLpAmount * scaleFactor / 10**PRECISION;
         totalWeights -= lpInfo.weight;
         totalLiquidity -= liquidityRemoved;
         lpInfo.removedLiquidity = true;
-        lpInfo.reassignableAfter = uint32(block.number) + LOAN_TENOR + GRACE_PERIOD;
+        lpInfo.reassignableAfter = uint32(blockNum) + LOAN_TENOR + GRACE_PERIOD;
         //ERC20 transfer liquidityRemoved
     }
 
     function borrow(uint256 _pledgeAmount, uint256 _minLoan, uint256 _maxRepay, uint256 _deadline) external {
         uint256 blockNum = block.number;
-        require(_deadline < blockNum, "after deadline");
+        require(blockNum < _deadline, "after deadline");
         require(_pledgeAmount > 0, "must pledge > 0");
         uint256 loanAmount = _pledgeAmount * maxLoanPerColl * totalLiquidity / (_pledgeAmount * maxLoanPerColl + totalLiquidity * 10**COLL_DECIMALS);
         require(loanAmount > _minLoan, "below _minLoan limit");
@@ -110,7 +121,8 @@ contract Contract {
         loanInfo.expiry = uint32(blockNum) + LOAN_TENOR;
         loanInfo.lpSnapshot = lpSlots;
         loanInfo.totalWeights = totalWeights;
-        loanInfo.collAndRepayAmounts = (_pledgeAmount << 128) + uint128(repaymentAmount);
+        loanInfo.repayment = uint128(repaymentAmount);
+        loanInfo.collateral = uint128(_pledgeAmount);
         loanIdxToLoanInfo[loanIdx] = loanInfo;
         loanIdx += 1;
         totalLiquidity -= loanAmount;
@@ -118,48 +130,103 @@ contract Contract {
     }
 
     function repay(uint256 _loanIdx) external {
-        require(_loanIdx > 0, "loan id must be > 0");
+        require(_loanIdx > 0 && _loanIdx < loanIdx, "loan id out of bounds");
         LoanInfo storage loanInfo = loanIdxToLoanInfo[_loanIdx];
         require(loanInfo.borrower == msg.sender, "unauthorized repay");
         require(block.number + 1 < loanInfo.expiry, "loan expired");
         require(!loanInfo.repaid, "loan already repaid");
         require(block.number > loanInfo.expiry - LOAN_TENOR, "cannot repay in same block");
         loanInfo.repaid = true;
-        totalClaimableRepays += uint128(loanInfo.collAndRepayAmounts);
+        totalClaimableRepays += loanInfo.repayment;
         //ERC20 transfer repaid and collateral
     }
 
     function claim(uint256 _slotIdx, uint256[] calldata _loanIdxs) external {
+        require(_slotIdx < 256, "_slotIdx out of bounds");
+        uint256 arrayLen = _loanIdxs.length;
+        require(arrayLen > 0 && arrayLen < loanIdx, "_loanIdxs out of bounds");
         LpInfo memory lpInfo = slotIdxToLpInfo[_slotIdx];
         require(lpInfo.addr == msg.sender, "lp unentitled for slot");
         require(_loanIdxs[0] != 0, "loan idx must be > 0");
         require(_loanIdxs[0] > lpToMaxLoanIdxClaimed[keccak256(abi.encodePacked(_slotIdx,msg.sender))], "lp already claimed given slot");
         LoanInfo memory loanInfo = loanIdxToLoanInfo[_loanIdxs[0]];
-        uint256 repayments;
-        uint256 collateral;
-        uint256 check = uint256(1) << _slotIdx;
-        uint256 arrayLen = _loanIdxs.length;
-        uint256 prevLoanIdx;
-        for (uint128 i = 0; i < arrayLen; ++i) {
-            if(i > 0) {
-                require(_loanIdxs[i] > prevLoanIdx, "loan ids must be ascending");
-                loanInfo = loanIdxToLoanInfo[_loanIdxs[i]];
-            }
-            check &= loanInfo.lpSnapshot;
-            if (loanInfo.repaid) {
-                repayments += uint128(loanInfo.collAndRepayAmounts) * lpInfo.weight / loanInfo.totalWeights;
-            } else if (block.number > loanInfo.expiry) {
-                collateral += (loanInfo.collAndRepayAmounts >> 128) * lpInfo.weight / loanInfo.totalWeights;
-            }
-            prevLoanIdx = _loanIdxs[i];
-        }
-        check = (check >> _slotIdx) & uint256(1);
-        require(check == 1, "lp not entitled for all loan ids");
+        require(block.number > loanInfo.expiry, "loans must have expired");
+        uint256 checkSlot = uint256(1) << _slotIdx;
+        (uint256 repayments, uint256 collateral, uint256 checkInAll) = repayCollAndCheckMask(checkSlot, _loanIdxs, lpInfo.weight);
+        require(checkInAll == checkSlot, "slot not entitled to all loans");
         lpToMaxLoanIdxClaimed[keccak256(abi.encodePacked(_slotIdx,msg.sender))] = _loanIdxs[arrayLen-1];
         totalClaimableRepays -= repayments;
         //ERC20 transfer repayments and collateral
     }
 
-    function groupClaim(uint256[] calldata _slotIdx, uint256[] calldata _loanIdxs) external {
+    function claimOnGroup(uint256 _slotIdx, uint256[] calldata _slots, uint256[] calldata _loanIdxs) external {
+        uint256 slotArrayLen = _slots.length;
+        require(slotArrayLen > _slotIdx && slotArrayLen < 256, "slotArrayLen out of bounds");
+        LpInfo memory lpInfo = slotIdxToLpInfo[_slots[_slotIdx]];
+        require(lpInfo.addr == msg.sender, "unentitled lp");
+        uint256 loanArrayLen = _loanIdxs.length;
+        require(loanArrayLen > 0 && loanArrayLen < loanIdx, "loanArrayLen out of bounds");
+        GroupedClaimsInfo memory claimsInfo = slotsToClaimsInfo[keccak256(abi.encodePacked(_slots,_loanIdxs))];
+        lpToMaxLoanIdxClaimed[keccak256(abi.encodePacked(_slotIdx,msg.sender))] = _loanIdxs[loanArrayLen-1];
+        uint256 repayments = claimsInfo.repayments * lpInfo.weight / claimsInfo.groupWeight;
+        uint256 collateral = claimsInfo.collateral * lpInfo.weight / claimsInfo.groupWeight;
+        //ERC20 transfer repayments and collateral
+    }
+
+    function groupClaims(uint256[] calldata _slots, uint256[] calldata _loanIdxs) external {
+        (uint128 groupWeight, uint256 slots) = weightsAndSlots(_slots);
+        (uint256 repayments, uint256 collateral, uint256 checkMask) = repayCollAndCheckMask(slots, _loanIdxs, groupWeight);
+        uint256 prev = _loanIdxs[0];
+        require(slots == checkMask, "slots not entitled to all loans");
+        GroupedClaimsInfo memory claimsInfo;
+        claimsInfo.repayments = uint128(repayments) ;
+        claimsInfo.collateral = uint128(collateral);
+        claimsInfo.groupWeight = groupWeight;
+        claimsInfo.maxLoanIdx = uint32(prev);
+        slotsToClaimsInfo[keccak256(abi.encodePacked(_slots,_loanIdxs))] = claimsInfo;
+    }
+
+    function weightsAndSlots(uint256[] calldata _slots) internal view returns (uint128, uint256) {
+        uint256 slotArrayLen = _slots.length;
+        require(slotArrayLen < 256, "slotArrayLen out of bounds");
+        LpInfo memory lpInfo;
+        uint128 groupWeight;
+        uint256 slots;
+        uint256 prev;
+        for (uint128 i = 0; i < slotArrayLen; ++i) {
+            lpInfo = slotIdxToLpInfo[_slots[i]];
+            groupWeight += lpInfo.weight;
+            if(i > 0) {
+                require(_slots[i] > prev, "loan ids must be ascending");
+            }
+            slots |= uint256(1) << _slots[i];
+            prev = _slots[i];
+        }
+        return (groupWeight, slots);
+    }
+
+    function repayCollAndCheckMask(uint256 _slots, uint256[] calldata _loanIdxs, uint256 _weight) internal view returns (uint256, uint256, uint256) {
+        uint256 loanArrayLen = _loanIdxs.length;
+        require(loanArrayLen > 0 && loanArrayLen < loanIdx, "loanArrayLen out of bounds");
+        LoanInfo memory loanInfo;
+        require(block.number > loanInfo.expiry, "loans must have expired");
+        uint256 checkMask = _slots;
+        uint256 repayments;
+        uint256 collateral;
+        uint256 prev = _loanIdxs[0];
+        for (uint128 i = 0; i < loanArrayLen; ++i) {
+            if(i > 0) {
+                require(_loanIdxs[i] > prev, "loan ids must be ascending");
+            }
+            loanInfo = loanIdxToLoanInfo[_loanIdxs[i]];
+            checkMask &= loanInfo.lpSnapshot;
+            if (loanInfo.repaid) {
+                repayments += loanInfo.repayment * _weight / loanInfo.totalWeights;
+            } else {
+                collateral += loanInfo.collateral * _weight / loanInfo.totalWeights;
+            }
+            prev = _loanIdxs[i];
+        }
+        return (repayments, collateral, checkMask);
     }
 }
