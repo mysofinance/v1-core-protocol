3b1b3d3175ce82ac3e8b294300b65925075d6223
Jamie Pickett <jpickett713@gmail.com>
2023-01-12 13:12:28 -0500

    added extra notes

--- contracts/peripheral/AddLiquidityChecker.sol
+++ contracts/peripheral/AddLiquidityChecker.sol
@@ -5,10 +5,14 @@ pragma solidity 0.8.17;
 import {IAddLiquidityChecker} from "../interfaces/IAddLiquidityChecker.sol";
 
 /**@dev this contract needs to implement any allowedToAdd
-// just have it return true to allow any adder, else implement any logic
-// you want in that function to have more fine-grained add control
-// example here is a whitelist mapping (could only be one address for one particular adder)
-// but in principle if you put complex logic or even require a seperate contract to call which checked
+// just have it return true to allow any adder,
+// return false to only allow creator to add,
+// else implement any logic you want
+// to have more fine-grained add control
+// example here is a whitelist mapping
+// (could only be one address for one particular adder)
+// but in principle if you put complex logic or even require
+// a seperate contract to call addLiquidity which checked
 // a merkle tree white (or black) list or whatever you wanted
 */
 
